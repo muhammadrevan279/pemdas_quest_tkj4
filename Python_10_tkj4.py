c9029a18138d704851e49456f1a1fa9e7c56eb82
muhammadrevan279 <115441481+muhammadrevan279@users.noreply.github.com>
2022-10-10 10:48:35 +0700

    Add files via upload

---
+++ Python_10_tkj4.py
@@ -0,0 +1,25 @@
+# membuat Tipe data koleksi
+
+koleksi_data_str =["pisang", "mangga", "jambu", "semangka"]
+
+koleksi_data_int =[200, 300, 400, 500]
+
+koleksi_data_mix =["Rizky Billar", 100, True, "Reza Arap"]
+
+print("Koleksi data String:", koleksi_data_str)
+
+print("Koleksi data Integer:", koleksi_data_int)
+
+print("Koleksi data Campuran:", koleksi_data_mix)
+
+koleksi_nama_hewan =["jerapah", "gajah", "macan"]
+
+koleksi_nilai_uts =[80, 70, 90]
+
+koleksi_nama_teman =["untung", "isal", "ridho"]
+
+print("Koleksi nama Hewan:", koleksi_nama_hewan)
+
+print("koleksi nilai UTS:", koleksi_nilai_uts)
+
+print("koleksi nama Teman:", koleksi_nama_teman)
